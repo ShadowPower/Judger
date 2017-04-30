9d2b2aaea952fbeec66beb5573422b1c5d09f359
virusdefender <virusdefender@outlook.com>
2017-04-30 13:17:38 +0800

    fix Python3 print

--- tests/Python_and_core/test.py
+++ tests/Python_and_core/test.py
@@ -1,4 +1,5 @@
 # coding=utf-8
+from __future__ import print_function
 import _judger
 from unittest import TestCase, main
 
@@ -6,5 +7,5 @@ from testcase.integration.test import IntegrationTest
 from testcase.seccomp.test import SeccompTest
 
 ver = _judger.VERSION
-print "Judger version %d.%d.%d" % ((ver >> 16) & 0xff, (ver >> 8) & 0xff, ver & 0xff)
+print("Judger version %d.%d.%d" % ((ver >> 16) & 0xff, (ver >> 8) & 0xff, ver & 0xff))
 main()
